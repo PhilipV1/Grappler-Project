90c845d636327f83f4866c14e1bd89b012d33c5e
PhilipV1 <philipvelandria@gmail.com>
2020-09-03 10:31:03 +0200

    Gun and projectiles
    Added a gun and projectile script. Player can now shoot a projectile from the gun which uses a rigidbody with an impulse force.

---
+++ Grappler/Assets/Scripts/PlayerGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gunBarrel;
+    public GameObject projPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            FireProjectile();
+        }
+    }
+
+    void FireProjectile()
+    {
+        GameObject temp = projPrefab;
+        temp = Instantiate(temp, gunBarrel.transform.position, this.transform.rotation);
+    }
+}
